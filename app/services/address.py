30b3eae0068eabb541a528c075526f756bad8632
csonfack <sonfackyann@gmail.com>
2024-08-17 21:30:12 +0100

    update

--- app/services/address.py
+++ app/services/address.py
@@ -17,6 +17,25 @@ async def get_address(
     
     if country:
         query &= Q(country__icontains=country)
+
+    if not city:  # Return only distinct countries if city is not provided
+        distinct_countries = Address.objects(query).distinct('country')
+        total_items = len(distinct_countries)
+        total_pages = math.ceil(total_items / page_size)
+        skip = (page - 1) * page_size
+
+        # Paginate the list of distinct countries
+        paginated_countries = distinct_countries[skip:skip + page_size]
+        
+        return PaginationDTO[Dict[str, Any]](
+            total_items=total_items,
+            total_pages=total_pages,
+            current_page=page,
+            page_size=page_size,
+            has_next=page < total_pages,
+            has_previous=page > 1,
+            items=[{'country': country} for country in paginated_countries]
+        )
     
     skip = (page - 1) * page_size
     results = Address.objects(query).skip(skip).limit(page_size)
